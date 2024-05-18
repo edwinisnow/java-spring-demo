3494eda10fd697784ad21fe5119e21cea57fda7a
EDWIN GEORGE LAZER <edwin04@gmail.com>
2024-05-18 07:01:21 +0100

    injecting objects

--- src/org/koushik/javabrains/Triangle.java
+++ src/org/koushik/javabrains/Triangle.java
@@ -1,32 +1,42 @@
 package org.koushik.javabrains;
 
 public class Triangle {
-	private String type;
-	private int height;
 	
-	public int getHeight() {
-		return height;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public Triangle(String type) {
-		this.type = type;
-	}
-	
-	public Triangle(int height) {
-		this.height = height;
-	}
-	
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
-	}
-	
-	public String getType() {
-		return type;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	
+	
 	public void draw() {
-		System.out.println("Triangle drawn : " + getType() + " height " + getHeight());
+		System.out.println("Point A = " + getPointA().getX() + getPointA().getY());
+		System.out.println("Point B = " + getPointB().getX() + getPointB().getY());
+		System.out.println("Point C = " + getPointC().getX() + getPointC().getY());
+
 	}
 
 }
